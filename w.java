45f5924bd4096f7bf50823145cf48f8964f50c2a
731613205024 <731613205024@users.noreply.github.com>
2016-07-18 14:02:06 +0530

    Add files via upload

---
+++ w.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aswinaji;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Student
+ */
+public class w {
+ 
+  
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+ 
+         Scanner s=new Scanner(System.in);
+        int t;int f=0;
+        int[] ab=new int[5];
+        System.out.println("enter array of 5 elements");
+        for(int i=0;i<5;i++)
+        {
+            ab[i]=s.nextInt();
+        }
+        System.out.println("enter ur target number"); 
+        t=s.nextInt();
+          for(int i=0;i<5;i++)
+          {
+         for(int j=i;j<5;j++)
+         {
+             if(ab[i]+ab[j]==t)
+             {  f++;
+                 System.out.println(ab[i]+"+ "+ab[j]+"= "+t);}
+    
+          }}
+          if(f==0)
+              System.out.println("no numbers in array will give target while adding");
+        
+    }
+        }
+    
+
+            
+        
+    
